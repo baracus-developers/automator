230cd4f0ac5bc48fd5917e94b8baa942151c39c1
Gregory Haskins <ghaskins@linux-mp.laurelwood.net>
2010-08-11 16:25:11 -0400

    Clean up event logging
    Signed-off-by: Gregory Haskins <ghaskins@linux-mp.laurelwood.net>

--- monitor/mon_app.erl
+++ monitor/mon_app.erl
@@ -10,8 +10,8 @@ start(_Type, _StartArgs) ->
     erlang:set_cookie(node(), list_to_atom(Cookie)),
 
     Ret = mon_sup:start_link(),
-    gen_event:add_handler(machine_events, machine_logger, []),
-    gen_event:add_handler(host_events, host_logger, []),
+    gen_event:add_handler(machine_events, event_logger, []),
+    gen_event:add_handler(host_events, event_logger, []),
     gen_event:add_handler(host_events, host_state, []),
     gen_event:add_handler(machine_events, host_state, []),
     gen_event:add_handler(host_events, provisioner, []),
