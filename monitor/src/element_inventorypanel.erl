37d59ea7845cbfe4732b249e7a0e076745295925
Gregory Haskins <ghaskins@novell.com>
2010-09-27 16:46:44 -0400

    Enhance the inventory tab
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- monitor/src/element_inventorypanel.erl
+++ monitor/src/element_inventorypanel.erl
@@ -1,5 +1,5 @@
 -module(element_inventorypanel).
--export([reflect/0, render_element/1]).
+-export([reflect/0, render_element/1, event/1]).
 
 -include_lib("nitrogen/include/wf.inc").
 -include_lib("hostinfo.hrl").
@@ -11,9 +11,16 @@ get_host(Host) ->
     {ok, HostInfo} = hosts_server:get_hostinfo(Host),
     [Host, atom_to_list(HostInfo#hostinfo.state)].
 
-get_data() ->
+get_hosts() ->
     {ok, Hosts} = hosts_server:enum(),
     [get_host(Host) || Host <- Hosts].
+
+get_pools() ->
+    [
+     "Default",
+     "Unassigned",
+     "High End"
+    ].
 
 render_rows(Data) ->
     Map = [
@@ -28,6 +35,7 @@ render_rows(Data) ->
            body=#tablerow {
              id=row,
              cells=[
+		    #tablecell { body=#checkbox{} },
                     #tablecell { id=macLabel },
                     #tablecell { id=statusLabel }
                    ]
@@ -61,13 +69,15 @@ alternate_color([Id, Status]=DataRow, Acc) ->
     }.
 
 render_table() ->
-    #table{ rows=[
+    #table{ class="nodes",
+	    rows=[
 		  #tablerow {cells=[
+				    #tableheader { text="Select" },
 				    #tableheader { text="MAC" },
 				    #tableheader { text="Status" }
 				   ]
 			    },
-		  render_rows(get_data())
+		  render_rows(get_hosts())
 		 ]
 	  }.
 
@@ -75,10 +85,42 @@ handle_event(Id, Event) ->
     wf:update(Id, render_table()),
     wf:flush().
 
-render_element(R) ->
-    Id = wf:temp_id(),
-    comet_event_relay:add_handler(host_events, Id,
-				  fun(Event) -> handle_event(Id, Event) end),
+render_pools() ->
+    #backsplash{ body=[
+		       #h1{text="Pools"},
+		       #list{body=[#listitem{text=Name} || Name <- get_pools()]}
+		      ]
+	       }.
 
-    Panel = #panel{ body=#panel{ id=Id, body=render_table()}},
+render_nodes() ->
+    Id = wf:temp_id(),
+    comet_event_relay:add_handler(host_events, Id ++ "-hosts",
+				  fun(Event) -> handle_event(Id, Event) end),   
+    comet_event_relay:add_handler(machine_events, Id ++ "-machines",
+				  fun(Event) -> handle_event(Id, Event) end),   
+    #backsplash{ body=[
+		       #h1{ text="Nodes"},
+		       #dropdown{id=select_pool,
+				 value="Default",
+				 postback=select_pool,
+				 delegate=?MODULE,
+				 options=[
+					  #option{text=Name, value=Name}
+					  || Name <- get_pools()
+					 ]
+				},
+		       #panel{ id=Id, body = render_table()}
+		      ]
+	       }.
+
+render_element(R) ->
+    Panel = #panel{ body=[
+			 render_pools(),
+			 render_nodes()
+			 ]
+		  },
     element_panel:render_element(Panel).
+
+event(select_pool) ->
+    Pool = wf:q(select_pool),
+    io:format("Selected: ~p~n", [Pool]).
