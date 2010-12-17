cb3f92aec986e1f671d425d27f1c42345370548d
Gregory Haskins <ghaskins@novell.com>
2010-12-17 10:57:56 -0500

    Add proper support for "service cloudbuilderd [stop|status]"
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- monitor/core/src/mon_app.erl
+++ monitor/core/src/mon_app.erl
@@ -3,6 +3,16 @@
 -export([start/2, stop/1, run_once/0]).
 
 start(_Type, _StartArgs) ->
+    case init:get_argument(pidfile) of
+    {ok, [PidFile]} ->
+        case file:write_file(PidFile, os:getpid()) of
+	    ok -> ok;
+	    Error ->
+		io:format("Failed to write PID file ~s, error: ~p", [PidFile, Error])
+        end;
+	_ -> ok
+    end,
+
     Ret = mon_sup:start_link(),
     gen_event:add_handler(machine_events, event_logger, []),
     gen_event:add_handler(host_events, event_logger, []),
